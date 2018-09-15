4e2a5ced75ba44a57b3ac4642907c8ec3783e6c9
Nasser Sanou <nassersanou23@gmail.com>
2018-09-15 03:02:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const shout = (string) => {
   return string.toUpperCase();
-  console.log(string.toUpperCase())
+  // console.log(string.toUpperCase())
 }
 // shout();
 
 whisper = (string) => {
   return string.toLowerCase()
-  console.log(string.toLowerCase())
+  // console.log(string.toLowerCase())
 }
 
 // mixedCase return half toLowerCase & half to toUpperCase
-
+const logShout = (string) => {
+  console.log(string.toUpperCase())
+}
